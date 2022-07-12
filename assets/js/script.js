ab1a53935885878efc80fe9fb7aeb511284fd945
ANDO Andriamalala <andriamalala5056@gmail.com>
2022-07-12 18:11:52 +0300

    contents

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,3 +41,18 @@ new Swiper('.gallery-slider', {
         }
     }
 });
+
+// navbar background
+
+
+window.onscroll = function() {scrollFunction()};
+ 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+
+    document.getElementById("navbar").style.background = "#f8f9fa";
+  } else {
+   
+    document.getElementById("navbar").style.background = "none";
+  }
+}
